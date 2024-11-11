e5e7b1b1d395dc144915e54e284464186a49e908
Aakamsh PM <aakamshpm@gmail.com>
2024-11-11 22:59:03 +0530

    models added, farmer model updated

---
+++ backend/models/Farmer.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const farmerSchema = mongoose.Schema(
+  {
+    // Personal Details
+    name: { type: String, required: true },
+    phoneNumber: { type: Number, required: true },
+    password: { type: String, required: true },
+    address: {
+      street: { type: String },
+      city: { type: String },
+      state: { type: String },
+      country: { type: String, default: "India" },
+      postalCode: { type: String },
+    },
+    profilePicture: { type: String, default: null },
+    bio: { type: String, maxLength: 500 },
+
+    // Farm Details
+    farmName: { type: String },
+    farmLocation: {
+      latitude: { type: Number },
+      longitude: { type: Number },
+    },
+    farmSizeInAcres: { type: Number },
+    cropsGrown: [{ type: String }],
+
+    // Product Details
+    products: [
+      {
+        name: { type: String },
+        description: { type: String },
+        pricePerKg: { type: Number },
+        quantityAvailableInKg: { type: String },
+        category: { type: String },
+        harvestDate: { type: Date },
+        images: [{ type: String }],
+      },
+    ],
+
+    // Transaction Details
+    transactions: [
+      {
+        buyerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        productId: { type: mongoose.Schema.Types.ObjectId },
+        quantitySold: { type: Number },
+        totalAmount: { type: Number },
+        status: {
+          type: String,
+          enum: ["Pending", "Completed", "Cancelled"],
+          default: "Pending",
+        },
+        date: { type: Date, default: Date.now },
+      },
+    ],
+  },
+
+  { timestamps: true }
+);
+
+const Farmer = mongoose.model("Farmer", farmerSchema);
+export default Farmer;
