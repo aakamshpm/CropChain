cda635be53dacec73bb95187669beb8c520142e9
Aakamsh PM <aakamshpm@gmail.com>
2024-11-11 23:46:44 +0530

    farmer registration controller added

--- backend/controllers/farmerController.js
+++ backend/controllers/farmerController.js
@@ -1,5 +1,49 @@
 import asyncHandler from "express-async-handler";
+import bcrypt from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
+import Farmer from "../models/Farmer.js";
 
+//register Farmer
+const farmerRegister = asyncHandler(async (req, res) => {
+  const { name, phoneNumber, password } = req.body;
+  if ((!name, !phoneNumber, !password)) {
+    res.status(400);
+    throw new Error("Please fill every fields");
+  }
+  const farmerExists = await Farmer.findOne({ phoneNumber });
+  if (farmerExists) {
+    res.status(400);
+    throw new Error("Account already exists");
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  try {
+    const farmer = await Farmer.create({
+      name,
+      phoneNumber,
+      password: hashedPassword,
+    });
+    if (farmer) {
+      generateToken(res, farmer._id, "farmer");
+      res.status(200).json({
+        message: "Farmer registration successful",
+        data: {
+          id: farmer._id,
+          name: farmer.name,
+          phoneNumber: farmer.phoneNumber,
+        },
+      });
+    } else {
+      res.status(400);
+      throw new Error("Farmer registration faileed");
+    }
+  } catch (err) {
+    res.status(500);
+    throw new Error(`Error Occurred: ${err.message}`);
+  }
+});
+
+//auth Farmer
 const farmerLogin = asyncHandler(async (req, res) => {});
 
-export { farmerLogin };
+export { farmerLogin, farmerRegister };
--- backend/routes/farmerRoutes.js
+++ backend/routes/farmerRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { farmerLogin } from "../controllers/farmerController.js";
+import { farmerRegister } from "../controllers/farmerController.js";
 
 const router = express.Router();
 
+router.post("/register", farmerRegister);
 router.post("/auth", farmerLogin);
 
 export default router;
