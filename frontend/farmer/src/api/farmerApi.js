94f88e3ca6e9f5e2d5a0fe0afdeb6e1157e938ba
Aakamsh PM <aakamshpm@gmail.com>
2024-12-14 23:31:25 +0530

    farmer api functions written in frontend

--- frontend/farmer/src/api/farmerApi.js
+++ frontend/farmer/src/api/farmerApi.js
@@ -10,4 +10,36 @@ const farmerRegister = async (credentials) => {
   return response.data;
 };
 
-export { farmerLogin, farmerRegister };
+const getFarmerDetails = async () => {
+  const response = await api.post("farmer/");
+  return response.data;
+};
+
+const updateAddress = async (data) => {
+  const response = await api.post("farmer/update-address", data);
+  return response.data;
+};
+
+const uploadProfilePicture = async (file) => {
+  const formData = new FormData();
+  formData.append("profilePicture", file);
+
+  const response = await api.post("farmer/upload-profile-picture", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+  return response.data;
+};
+
+const addFarm = async (data) => {
+  const response = await api.post("farmer/add-farm", data);
+  return response.data;
+};
+
+export {
+  farmerLogin,
+  farmerRegister,
+  getFarmerDetails,
+  uploadProfilePicture,
+  updateAddress,
+  addFarm,
+};
